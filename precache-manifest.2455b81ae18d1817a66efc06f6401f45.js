f92c4c50581d0c390284c051d26245866dccf4d8
Bojan Krstic <krstic.bojan80@gmail.com>
2019-06-05 13:36:40 +0200

    Updates

---
+++ precache-manifest.2455b81ae18d1817a66efc06f6401f45.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5aee20aebd0fe0ad75053dbbe374aa0",
+    "url": "/online-commerce/index.html"
+  },
+  {
+    "revision": "7d02287ec6666af74d14",
+    "url": "/online-commerce/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "10e3deca037ad9991b3a",
+    "url": "/online-commerce/static/css/main.8ff5aa91.chunk.css"
+  },
+  {
+    "revision": "7d02287ec6666af74d14",
+    "url": "/online-commerce/static/js/2.59d48d2f.chunk.js"
+  },
+  {
+    "revision": "10e3deca037ad9991b3a",
+    "url": "/online-commerce/static/js/main.8f14e67c.chunk.js"
+  },
+  {
+    "revision": "e46f0452103889b7f545",
+    "url": "/online-commerce/static/js/runtime~main.ad5c91c5.js"
+  },
+  {
+    "revision": "ecca523808d3a27b6b2c3196d84a8112",
+    "url": "/online-commerce/static/media/alcatel.ecca5238.jpg"
+  },
+  {
+    "revision": "2c4f207b9f48a4febd1a00cc09468b19",
+    "url": "/online-commerce/static/media/alcatel1.2c4f207b.jpg"
+  },
+  {
+    "revision": "2b5903d21b1426c78c1cf1ef7a4e79ef",
+    "url": "/online-commerce/static/media/alcatel2.2b5903d2.jpg"
+  },
+  {
+    "revision": "94fb90cd02732c6a47e01b850611a16a",
+    "url": "/online-commerce/static/media/htc.94fb90cd.jpg"
+  },
+  {
+    "revision": "162c429024f90efe8c7047f67e0f6b9a",
+    "url": "/online-commerce/static/media/htc1.162c4290.jpg"
+  },
+  {
+    "revision": "286ff296ec3e95ddf88e46f67659849f",
+    "url": "/online-commerce/static/media/htc2.286ff296.jpg"
+  },
+  {
+    "revision": "6418ed6d391b3cb7a80d64135c4b377a",
+    "url": "/online-commerce/static/media/huawei.6418ed6d.jpg"
+  },
+  {
+    "revision": "0a263ed888938ed2e3528c8b70133fb0",
+    "url": "/online-commerce/static/media/huawei1.0a263ed8.jpg"
+  },
+  {
+    "revision": "f0f5805e98f51ee559be14fad695129a",
+    "url": "/online-commerce/static/media/huawei2.f0f5805e.jpg"
+  },
+  {
+    "revision": "b25b46e818bbecf0996519147593546c",
+    "url": "/online-commerce/static/media/iPhone.b25b46e8.jpg"
+  },
+  {
+    "revision": "aa65699c2875b85daf89693b458cd2a3",
+    "url": "/online-commerce/static/media/iPhone1.aa65699c.jpg"
+  },
+  {
+    "revision": "6d28a24cbe3d54cbd1081cca99f6d3bb",
+    "url": "/online-commerce/static/media/iPhone2.6d28a24c.jpg"
+  },
+  {
+    "revision": "e2230ca7a61fa61e8f48a01862067d51",
+    "url": "/online-commerce/static/media/motorola.e2230ca7.jpg"
+  },
+  {
+    "revision": "553d0129d716909cddbed7619d91bdd7",
+    "url": "/online-commerce/static/media/motorola1.553d0129.jpg"
+  },
+  {
+    "revision": "40de1d154e3ce34b9e95be9f30a8a0f3",
+    "url": "/online-commerce/static/media/motorola2.40de1d15.jpg"
+  },
+  {
+    "revision": "534f319ce1ca7d76eca2d21dedf38ab1",
+    "url": "/online-commerce/static/media/nokia.534f319c.jpg"
+  },
+  {
+    "revision": "1026645c54fa4446c68ea771abbccf74",
+    "url": "/online-commerce/static/media/nokia1.1026645c.jpg"
+  },
+  {
+    "revision": "85fde05eb0fcdb1aa12a95412f5de330",
+    "url": "/online-commerce/static/media/nokia2.85fde05e.jpg"
+  },
+  {
+    "revision": "f40e67eb6c991d8a1c27eb79dd31fc94",
+    "url": "/online-commerce/static/media/onlineShoping.f40e67eb.jpg"
+  },
+  {
+    "revision": "8b77ce626f66e62699ac0d769f39facc",
+    "url": "/online-commerce/static/media/phone.8b77ce62.svg"
+  },
+  {
+    "revision": "7284cc57fdbdc3609906644bc26de0a5",
+    "url": "/online-commerce/static/media/samsung.7284cc57.jpg"
+  },
+  {
+    "revision": "a9e47fa4c6be2362a4d382f9dc2afbcc",
+    "url": "/online-commerce/static/media/samsung1.a9e47fa4.jpg"
+  },
+  {
+    "revision": "39b2048f9bdfc589f5213d4d38e792e7",
+    "url": "/online-commerce/static/media/samsung2.39b2048f.jpg"
+  },
+  {
+    "revision": "5facf91c6594a03891c656a15b122599",
+    "url": "/online-commerce/static/media/sony.5facf91c.jpg"
+  },
+  {
+    "revision": "3d499c1f8cd4d8e59945aa8f91b6abca",
+    "url": "/online-commerce/static/media/sony1.3d499c1f.jpg"
+  },
+  {
+    "revision": "0ff39d0c64d6ec607f29f1132e1bf5b0",
+    "url": "/online-commerce/static/media/sony2.0ff39d0c.jpg"
+  }
+]);
